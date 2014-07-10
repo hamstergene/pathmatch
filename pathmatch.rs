270e0930d435b15a776f0ad47870b3966605a793
Eugene Homyakov <ehomyakov@gmail.com>
2014-07-10 19:32:15 +0400

    tests for any character wildcard `?`

--- pathmatch.rs
+++ pathmatch.rs
@@ -1,6 +1,35 @@
 pub fn pathmatch(pattern: &str, pathstring: &str) -> bool
 {
-    return pattern == pathstring
+    let mut pattern_chars = pattern.chars();
+    let mut path_chars = pathstring.chars();
+    loop {
+        match pattern_chars.next() {
+            None => return path_chars.next().is_none(),
+            Some(pc) => {
+                match pc {
+                    '?' => return path_chars.next().is_some(),
+                    _ => {
+                        match path_chars.next() {
+                            None => return false,
+                            Some(sc) => if pc != sc { return false },
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[test]
+fn pathmatch_test_anychar()
+{
+    assert!(pathmatch("?", "?"));
+    assert!(pathmatch("?", "a"));
+    assert!(pathmatch("??", "BC"));
+    assert!(pathmatch("B?", "BC"));
+    assert!(pathmatch("?C", "BC"));
+    assert!(!pathmatch("?", ""));
+    assert!(!pathmatch("D?", "BC"));
 }
 
 #[test]
