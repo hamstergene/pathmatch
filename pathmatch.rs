52c73795490f3cb4112e8a3f545d51415b9b5584
Eugene Homyakov <ehomyakov@gmail.com>
2014-07-10 18:51:01 +0400

    exact equality tests

--- pathmatch.rs
+++ pathmatch.rs
@@ -1,6 +1,15 @@
-#[test]
-fn pathmatch_test_fail()
+pub fn pathmatch(pattern: &str, pathstring: &str) -> bool
 {
-    assert_eq!(2i, 3i);
+    return pattern == pathstring
+}
+
+#[test]
+fn pathmatch_test_exact_equality()
+{
+    assert!(pathmatch("", ""));
+    assert!(pathmatch("a", "a"));
+    assert!(pathmatch("BC", "BC"));
+    assert!(!pathmatch("a", ""));
+    assert!(!pathmatch("", "BC"));
 }
 
