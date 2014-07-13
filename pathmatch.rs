b1b8a6d74e2bc017cd7b8de507139dc7f0bfc48d
Eugene Homyakov <ehomyakov@gmail.com>
2014-07-13 22:42:29 +0400

    test for special cases of anypath

--- pathmatch.rs
+++ pathmatch.rs
@@ -7,7 +7,20 @@ pub fn pathmatch(pattern: &str, pathstring: &str) -> bool
 
     fn pathmatch_impl(mut pattern_chars: Chars, mut path_chars: Chars) -> bool
     {
+        if char_iter_equals(pattern_chars, "**/", true) {
+            // How can I convert Skip<Chars> to Chars to make use of .skip(3) ?
+            let mut pattern_chars_copy = pattern_chars;
+            pattern_chars_copy.next();
+            pattern_chars_copy.next();
+            pattern_chars_copy.next();
+            if pathmatch_impl(pattern_chars_copy, path_chars) {
+                return true;
+            }
+        }
         loop {
+            if char_iter_equals(pattern_chars, "/**", false) && path_chars.peekable().is_empty() {
+                return true;
+            }
             match pattern_chars.next() {
                 None => return path_chars.next().is_none(),
                 Some('?') => match path_chars.next() {
@@ -38,6 +51,15 @@ pub fn pathmatch(pattern: &str, pathstring: &str) -> bool
                         }
                     },
                 },
+                Some('/') => {
+                    match path_chars.next() {
+                        None => return false,
+                        Some(sc) => if sc != '/' { return false },
+                    }
+                    if char_iter_equals(pattern_chars, "**/", true) && pathmatch_impl(pattern_chars, path_chars) {
+                        return true;
+                    }
+                },
                 Some(pc) => {
                     match path_chars.next() {
                         None => return false,
@@ -47,6 +69,52 @@ pub fn pathmatch(pattern: &str, pathstring: &str) -> bool
             }
         }
     }
+
+    fn char_iter_equals(mut char_iter: Chars, needle: &str, startswith: bool) -> bool
+    {
+        for needle_char in needle.chars() {
+            match char_iter.next() {
+                Some(x) if x == needle_char => (),
+                _ => return false,
+            }
+        }
+        return if startswith { true } else { char_iter.next().is_none() };
+    }
+}
+
+#[test]
+fn pathmatch_test_collapse()
+{
+    // `/**/` may match a single slash.
+    assert!(pathmatch("foo/**/bar", "foo/bar"));
+    assert!(pathmatch("foo/**/bar", "foo//bar"));
+    assert!(pathmatch("foo/*/**/bar", "foo//bar"));
+    assert!(!pathmatch("foo/*/**/bar", "foo/bar"));
+    assert!(!pathmatch("foo/**/*/bar", "foo/bar"));
+    assert!(pathmatch("foo/**/", "foo/"));
+    assert!(!pathmatch("foo/**/", "foo"));
+    assert!(pathmatch("/**/bar", "/bar"));
+    assert!(!pathmatch("/**/bar", "bar"));
+}
+
+#[test]
+fn pathmatch_test_anypath_leading()
+{
+    // `**/` at the beginning of pattern may match nothing or a slash.
+    assert!(pathmatch("**/bar", "bar"));
+    assert!(pathmatch("**/bar", "/bar"));
+    assert!(!pathmatch("a**/bar", "abar"));
+    assert!(!pathmatch("*/bar", "bar")); // does not apply to `*/`
+}
+
+#[test]
+fn pathmatch_test_anypath_trailing()
+{
+    // `/**` at the end of pattern may match nothing or a slash.
+    assert!(pathmatch("foo/**", "foo"));
+    assert!(pathmatch("foo/**", "foo/"));
+    assert!(!pathmatch("foo/**g", "foog"));
+    assert!(!pathmatch("foo/*", "foo")); // does not apply to `/*`
 }
 
 #[test]
