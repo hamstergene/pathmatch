c3793742149d570b81b4ecffdce12e37a776f8d6
Eugene Homyakov <ehomyakov@gmail.com>
2014-07-17 23:18:09 +0400

    pmfind command line tool to play with patterns on real filesystem

--- Makefile
+++ Makefile
@@ -1,9 +1,16 @@
-all : test
+.PHONY : all test clean
 
-test : ./test-pathmatch
+all : pmfind test
+
+pmfind : pmfind.rs pathmatch.rs Makefile
+	rustc -g -o "$@" "$<"
+
+test : test-pathmatch
 	./test-pathmatch
 
-./test-pathmatch : pathmatch.rs Makefile
+test-pathmatch : pathmatch.rs Makefile
 	rustc -g --test -o "$@" "$<"
 
+clean :
+	rm -rf test-pathmatch pmfind test-pathmatch.dSYM pmfind.dSYM
 
---
+++ pmfind.rs
@@ -0,0 +1,54 @@
+#![feature(macro_rules)]
+extern crate getopts;
+use std::os;
+mod pathmatch;
+
+fn main()
+{
+    let args = os::args();
+    let program = args.get(0).clone();
+    let optspec: &[getopts::OptGroup] = [
+        getopts::optopt("C", "dir", "change directory to this before starting", "dir"),
+        getopts::optflag("h", "help", "print help and exit"),
+        ];
+    match getopts::getopts(args.tail(), optspec) {
+        Ok(matches) => {
+            if matches.opt_present("h") {
+                println!("{}", getopts::usage(format!("Print subpaths of current directory that match given pathmatch() patterns\n\nUsage:\n\t{} [options] [pattern ...]", program).as_slice(), optspec));
+            } else {
+                pmfind(&matches);
+            }
+        },
+        Err(x) => fail!(x.to_str()),
+    }
+}
+
+fn pmfind(matches: &getopts::Matches)
+{
+    match matches.opt_str("C") {
+        Some(dir) => if !os::change_dir(&Path::new(dir.clone())) { fail!("Can not chdir to: {}", dir); },
+        None => {},
+    }
+    match std::io::fs::walk_dir(&Path::new("")) {
+        Ok(mut dirs_iter) => {
+            for path in dirs_iter {
+                match path.as_str() {
+                    Some(path_str) => {
+                        let mut accept = (matches.free.len() == 0);
+                        for pattern_string in matches.free.iter() {
+                            if pathmatch::pathmatch(pattern_string.as_slice(), path_str) {
+                                accept = true;
+                                break;
+                            }
+                        }
+                        if accept { 
+                            println!("{}", path.display()); 
+                        }
+                    },
+                    None => {},
+                }
+            }
+        },
+        Err(res) => fail!("{}", res.to_str()),
+    }
+}
